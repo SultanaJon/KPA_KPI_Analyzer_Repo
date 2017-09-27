be4dc97fd6d7a7dac67b1a13cd5bf5a2523b09d9
Jon Sultana <jonsultana@outlook.com>
2017-09-27 18:17:42 -0400

    - Started to create the KPI dataTableLoader Classes for each section.

--- KPA_KPI Analyzer/DataLoading/KPI_Data/DataTableLoader/KpiDataTableLoader.cs
+++ KPA_KPI Analyzer/DataLoading/KPI_Data/DataTableLoader/KpiDataTableLoader.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace KPA_KPI_Analyzer.DataLoading.KPI_Data.DataTableLoader
 {
-    public class KpiDataTableLoader
+    public static class KpiDataTableLoader
     {
+        internal static class Plan
+        {
+
+        }
+
+
+        internal static class Purch
+        {
+
+        }
+
+
+        internal static class FollowUp
+        {
+
+        }
+
+        internal static class PlanTwo
+        {
+
+        }
+
+
+        internal static class PurchTwo
+        {
+
+        }
+
+
+        internal static class PurchSub
+        {
+
+        }
+
+
+        internal static class PurchTotal
+        {
+            
+        }
+
+
+        internal static class PurchPlan
+        {
+
+        }
+
+
+        internal static class PurchPlanTotal
+        {
+
+        }
+
+
+        internal static class Other
+        {
+
+        }
     }
 }
