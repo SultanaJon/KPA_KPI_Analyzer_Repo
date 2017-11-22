1a9b30dd79a217b8cd56cc1c8ec48ed0a231cab5
Jon Sultana <jonsultana@outlook.com>
2017-11-22 13:29:51 -0500

    Added the new assembly version.

--- KPA_KPI Analyzer/Properties/AssemblyInfo.cs
+++ KPA_KPI Analyzer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KPA_KPI Analyzer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Comau LLC")]
 [assembly: AssemblyProduct("KPA_KPI Analyzer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3.4")]
-[assembly: AssemblyFileVersion("1.1.3.4")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
